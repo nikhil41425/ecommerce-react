07b204f957cd3c87d04c99aa8db3a451e016a45c
unknown <nikhil41425@gmail.com>
2021-02-14 21:17:20 +0530

    updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,6 +2,8 @@ var JWT=require('jsonwebtoken');
 
 var UserModel=require('../models/user.model');
 var Config=require('../config/app.config');
+const { request, response } = require('express');
+const userModel = require('../models/user.model');
 
 exports.registerUser=(request,response)=>{
 
@@ -17,6 +19,61 @@ exports.registerUser=(request,response)=>{
             var payload={id:doc._id};
             var token=JWT.sign(payload,Config.config.JWT_SECRET);
             response.send({result:"success",token:token});
+        }
+    });
+}
+
+exports.loginUser=(request,response)=>{
+
+    var userData=request.body;
+
+    UserModel.findOne({emailId:userData.emailId},(err,doc)=>{
+
+        if(err){
+            console.log(err);
+            response.send({status:false,err:err.message});
+        }
+        if(doc){
+            if(doc.password == userData.password){
+                var payload=({id:doc._id});
+                var token=JWT.sign(payload,Config.config.JWT_SECRET);
+                response.send({status:true,token:token});
+            }
+        }
+       
+    })
+
+}
+
+exports.changePassword=(request,response)=>{
+
+    var userData=request.body;
+
+    UserModel.findOne({emailId:userData.emailId},(err,doc)=>{
+
+        if(err){
+            console.log(err);
+            response.send({status:false,err:err.message});
+        }
+        if(doc){
+            if(doc.password==userData.currentPassword){
+
+                UserModel.updateOne({emailId:userData.emailId},{password:userData.newPassword},(err,res)=>{
+
+                    if(err){
+                        console.log(err);
+                        response.send({status:false,err:err.message});
+                    }
+                    if(res){
+                        response.send({status:true, message:"updated"});
+                    }
+                    
+                });
+            }
+            else{
+                response.send({status:false,message:"current password incorrect"});
+            }
+
         }
     });
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,8 +1,36 @@
 var express=require('express');
 var userRouter=express.Router();
+var jwt=require('jsonwebtoken');
 
 var UserController=require('../controllers/user.controller');
+var Config=require('../config/app.config');
+const { request } = require('express');
+const { JsonWebTokenError } = require('jsonwebtoken');
+
+var authMiddleware=(request,response,next)=>{
+    console.log(request.headers);
+
+    if(request.headers.authorization==null || request.headers.authorization==""){
+        response.status(401).send("unauthorized access");
+    }
+
+    var token=request.headers.authorization.split(' ')[1];
+    console.log('token',token);
+
+    jwt.verify(token,Config.config.JWT_SECRET,function(err,payload){
+        if(err){
+            console.log({error:err.message});
+        }
+        if(payload){
+            console.log(payload);
+            next();
+        }
+    });
+}
 
 userRouter.post('/register',UserController.registerUser);
+userRouter.post('/login',UserController.loginUser);
+userRouter.post('/changePassword',authMiddleware,UserController.changePassword);
+
 
 module.exports=userRouter;
